9ea0e77fe1e0a74752aa42ccff0d73b982981035
Felipe Oliveira <flipeoliveira007@gmail.com>
2023-10-30 21:01:08 -0300

    add comment

---
+++ script/apresentation.js
@@ -0,0 +1,13 @@
+html = document.getElementsByClassName("fa-html5");
+
+function showNameIcon() {
+    // let element = document.getElementById("html-name");
+    // // element.style.display = "block";
+
+    // if(element.style.display === "none") {
+    //     element.style.display = "block";
+    // } else {
+    //     element.style.display = "none";
+    // }
+    // console.log("TESTE")
+}
