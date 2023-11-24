fe5da99f62a8f91ece012bbcaf1ec9610e60d01e
Felipe Oliveira <flipeoliveira007@gmail.com>
2023-11-23 23:47:50 -0300

    EMENIZANDO ERRO DA BiBLIOTECA REVEAL

--- script/scroll_reveal.js
+++ script/scroll_reveal.js
@@ -2,14 +2,14 @@ window.show = ScrollReveal({reset:true})
 
 show.reveal('.my-info-reveal', {
     duration: 1000,
-    distance: '70px',
+    distance: '10px',
     origin: 'left',
     delay: 200
 });
 
 show.reveal('.my-image-reveal', {
     duration: 1000,
-    distance: '70px',
+    distance: '10px',
     origin: 'right',
     delay: 200
 });
@@ -17,46 +17,46 @@ show.reveal('.my-image-reveal', {
 
 show.reveal('.my-skill-reveal', {
     duration: 1000,
-    distance: '70px',
+    distance: '10px',
     delay: 600
 });
 
 show.reveal('.title-reveal', {
     duration: 1000,
-    distance: '70px',
+    distance: '10px',
     delay: 800
 });
 
 show.reveal('.skills-reveal', {
     duration: 1000,
-    distance: '70px',
+    distance: '10px',
     delay: 1000
 });
 
 show.reveal('.project-left-reveal', {
     duration: 1000,
-    distance: '70px',
+    distance: '10px',
     origin: 'left',
     delay: 1200
 });
 
 show.reveal('.project-right-reveal', {
     duration: 1000,
-    distance: '70px',
+    distance: '10px',
     origin: 'right',
     delay: 1200
 });
 
 show.reveal('.certification-left-reveal', {
     duration: 1000,
-    distance: '70px',
+    distance: '10px',
     origin: 'left',
     delay: 900
 });
 
 show.reveal('.certification-reveal', {
     duration: 1000,
-    distance: '70px',
+    distance: '10px',
     origin: 'right',
     delay: 1000
 });
