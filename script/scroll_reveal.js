5bfd0284f2cddd98e31267a6650f7620b35e7e3f
Felipe Oliveira <flipeoliveira007@gmail.com>
2023-11-23 23:03:33 -0300

    feat: add script to animation reveal

---
+++ script/scroll_reveal.js
@@ -0,0 +1,68 @@
+window.show = ScrollReveal({reset:true})
+
+show.reveal('.my-info-reveal', {
+    duration: 1000,
+    distance: '70px',
+    origin: 'left',
+    delay: 200
+});
+
+show.reveal('.my-image-reveal', {
+    duration: 1000,
+    distance: '70px',
+    origin: 'right',
+    delay: 200
+});
+
+
+show.reveal('.my-skill-reveal', {
+    duration: 1000,
+    distance: '70px',
+    delay: 600
+});
+
+show.reveal('.title-reveal', {
+    duration: 1000,
+    distance: '70px',
+    delay: 800
+});
+
+show.reveal('.skills-reveal', {
+    duration: 1000,
+    distance: '70px',
+    delay: 1000
+});
+
+show.reveal('.project-left-reveal', {
+    duration: 1000,
+    distance: '70px',
+    origin: 'left',
+    delay: 1200
+});
+
+show.reveal('.project-right-reveal', {
+    duration: 1000,
+    distance: '70px',
+    origin: 'right',
+    delay: 1200
+});
+
+show.reveal('.certification-left-reveal', {
+    duration: 1000,
+    distance: '70px',
+    origin: 'left',
+    delay: 900
+});
+
+show.reveal('.certification-reveal', {
+    duration: 1000,
+    distance: '70px',
+    origin: 'right',
+    delay: 1000
+});
+
+show.reveal('.footer-reveal', {
+    duration: 1000,
+    distance: '70px',
+    delay: 1000
+});
