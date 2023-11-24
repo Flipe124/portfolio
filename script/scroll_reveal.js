fc742b1bbc32fc025b00e95dfb76d78358e87e6f
Felipe Oliveira <flipeoliveira007@gmail.com>
2023-11-24 09:56:51 -0300

    feat: add function RevealIntem()

--- script/scroll_reveal.js
+++ script/scroll_reveal.js
@@ -1,68 +1,85 @@
-window.show = ScrollReveal({reset:true})
+var widthScreen = window.innerWidth;
+RevealIntem(widthScreen);
 
-show.reveal('.my-info-reveal', {
-    duration: 1000,
-    distance: '10px',
-    origin: 'left',
-    delay: 200
+window.addEventListener('resize', function() {
+    location.reload();
 });
 
-show.reveal('.my-image-reveal', {
-    duration: 1000,
-    distance: '10px',
-    origin: 'right',
-    delay: 200
-});
+function RevealIntem(screen) {
+    var show = ScrollReveal({ reset: true });
+    var origin;
 
+    if(screen < 768) {
+        origin = "left";
+    } else {
+        origin = "right";
+    }
 
-show.reveal('.my-skill-reveal', {
-    duration: 1000,
-    distance: '10px',
-    delay: 600
-});
+    show.reveal('.my-info-reveal', {
+        duration: 1000,
+        distance: '10px',
+        origin: 'left', 
+        delay: 200
+    });
+    
+    show.reveal('.my-image-reveal', {
+        duration: 1000,
+        distance: '10px',
+        origin: origin,
+        delay: 400
+    });
+    
+    
+    show.reveal('.my-skill-reveal', {
+        duration: 1000,
+        distance: '10px',
+        delay: 600
+    });
+    
+    show.reveal('.title-reveal', {
+        duration: 1000,
+        distance: '10px',
+        delay: 800
+    });
+    
+    show.reveal('.skills-reveal', {
+        duration: 1000,
+        distance: '10px',
+        delay: 1000
+    });
+    
+    show.reveal('.project-left-reveal', {
+        duration: 1000,
+        distance: '10px',
+        origin: 'left',
+        delay: 1200
+    });
+    
+    show.reveal('.project-right-reveal', {
+        duration: 1000,
+        distance: '10px',
+        origin: origin,
+        delay: 1200
+    });
+    
+    show.reveal('.certification-left-reveal', {
+        duration: 1000,
+        distance: '10px',
+        origin: 'left',
+        delay: 900
+    });
+    
+    show.reveal('.certification-reveal', {
+        duration: 1000,
+        distance: '10px',
+        origin: origin,
+        delay: 1000
+    });
+    
+    show.reveal('.footer-reveal', {
+        duration: 1000,
+        distance: '20px',
+        delay: 1000
+    });
 
-show.reveal('.title-reveal', {
-    duration: 1000,
-    distance: '10px',
-    delay: 800
-});
-
-show.reveal('.skills-reveal', {
-    duration: 1000,
-    distance: '10px',
-    delay: 1000
-});
-
-show.reveal('.project-left-reveal', {
-    duration: 1000,
-    distance: '10px',
-    origin: 'left',
-    delay: 1200
-});
-
-show.reveal('.project-right-reveal', {
-    duration: 1000,
-    distance: '10px',
-    origin: 'right',
-    delay: 1200
-});
-
-show.reveal('.certification-left-reveal', {
-    duration: 1000,
-    distance: '10px',
-    origin: 'left',
-    delay: 900
-});
-
-show.reveal('.certification-reveal', {
-    duration: 1000,
-    distance: '10px',
-    origin: 'right',
-    delay: 1000
-});
-
-show.reveal('.footer-reveal', {
-    duration: 1000,
-    distance: '20px',
-    delay: 1000
-});
+}
