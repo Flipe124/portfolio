4411f45b69b80e78ba66f9b8a6597409e20ceb84
Felipe Oliveira <85032728+Flipe124@users.noreply.github.com>
2024-01-12 19:54:27 -0300

    feat: adding function to control navbar background

--- script/navbar.js
+++ script/navbar.js
@@ -28,3 +28,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+window.addEventListener('scroll', function () {
+    var navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 1) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
