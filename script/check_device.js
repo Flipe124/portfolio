573b3d524609a6b37c5b4e0db5250f88000845ed
Felipe Oliveira <85032728+Flipe124@users.noreply.github.com>
2024-01-16 22:29:58 -0300

    feat: adding check device

---
+++ script/check_device.js
@@ -0,0 +1,12 @@
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if(isMobileDevice) {
+    var sections = document.querySelectorAll("section");
+    console.log("Teste")
+
+    for(i = 0;i < sections.length; i++) {
+        sections[i].classList.add("mobile");
+    }
+}
